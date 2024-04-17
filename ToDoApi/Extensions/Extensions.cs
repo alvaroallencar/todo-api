26e4a974df558e92e24fd8e1ee52ef30d75e751e
Álvaro Romário Cavalcante Alencar <alvaroromario1@hotmail.com>
2024-04-17 15:09:27 -0300

    refactor: add IToDoRepository and ToDoRepository to extension method

--- ToDoApi/Extensions/Extensions.cs
+++ ToDoApi/Extensions/Extensions.cs
@@ -1,7 +1,9 @@
+using ToDoApi.Contracts;
+
 namespace ToDoApi.Extensions;
 
 public static class Extensions
 {
     public static IServiceCollection AddToDoServices(this IServiceCollection services)
-        => services;
+        => services.AddScoped<IToDoRepository, IToDoRepository>();
 }
