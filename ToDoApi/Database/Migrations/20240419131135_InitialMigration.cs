c9ea888a796971a6bbbac9d5607f60dc2cc2cbd4
Álvaro Romário Cavalcante Alencar <alvaroromario1@hotmail.com>
2024-04-19 10:40:47 -0300

    refactor: add db container and run migrations automatic

---
+++ ToDoApi/Database/Migrations/20240419131135_InitialMigration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoApi.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Priority = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -1,25 +1,55 @@
 using System.Text.Json.Serialization;
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ToDoApi.Database;
 using ToDoApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Loads Environment Variables
+Env.Load("../.env");
+
+// Adds support to convert enums
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
+
+// Adds swagger tags
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("todo-db"));
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ToDo API",
+        Version = "v1",
+        Description = "A simple api to manage tasks (ToDos)",
+    });
+});
+
+// Adds postgres database connection
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseNpgsql(Env.GetString("DB_CONNECTION_STRING")));
+
+// Adds To Do services
 builder.Services.AddToDoServices();
 
 var app = builder.Build();
 
+// Adds swagger in development environment
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+// Runs migrations when API starts
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<DataContext>();
+    dbContext.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
